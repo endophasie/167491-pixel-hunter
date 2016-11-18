dcab60e5deba8734e573d04492fc43ce5c1c7d3c
endophasie <albina.tokarchuk@gmail.com>
2016-11-18 16:29:00 +0700

    Remove empty string

--- js/main.js
+++ js/main.js
@@ -27,9 +27,7 @@
   let mainElement = document.getElementById('main');
 
   let switcher = document.createElement('div');
-  switcher.innerHTML = '' +
-    '<span class="prev"><img src="img/arrow_left.svg" alt="Left" width="50" height="50"></span>   ' +
-    '<span class="next"><img src="img/arrow_right.svg" alt="Right" width="50" height="50"></span>';
+  switcher.innerHTML = '<span class="prev"><img src="img/arrow_left.svg" alt="Left" width="50" height="50"></span> <span class="next"><img src="img/arrow_right.svg" alt="Right" width="50" height="50"></span>';
   switcher.style.cssText = 'text-align: center';
   mainElement.after(switcher);
 
