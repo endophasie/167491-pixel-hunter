3fb6f8e8f7f65ae9b5b2af0216b6ae23df6304b1
endophasie <albina.tokarchuk@gmail.com>
2016-12-07 13:44:23 +0700

    Fix states titles

--- js/blocks/gameStatsResult.js
+++ js/blocks/gameStatsResult.js
@@ -1,23 +1,18 @@
-const gameStatsData = {
-  wrong: 'stats__result--wrong',
-  slow: 'stats__result--slow',
-  fast: 'stats__result--fast',
-  correct: 'stats__result--correct',
-  base: 'stats__result--unknown'
+const gameStatsMap = {
+  wrong: 'wrong',
+  slow: 'slow',
+  fast: 'fast',
+  correct: 'correct',
+  base: 'unknown'
 };
 
 const gameStatsResult = `
           <ul class="stats">
-            <li class="stats__result ${gameStatsData.wrong}"></li>
-            <li class="stats__result ${gameStatsData.slow}"></li>
-            <li class="stats__result ${gameStatsData.fast}"></li>
-            <li class="stats__result ${gameStatsData.correct}"></li>
-            <li class="stats__result ${gameStatsData.base}"></li>
-            <li class="stats__result ${gameStatsData.base}"></li>
-            <li class="stats__result ${gameStatsData.base}"></li>
-            <li class="stats__result ${gameStatsData.base}"></li>
-            <li class="stats__result ${gameStatsData.base}"></li>
-            <li class="stats__result ${gameStatsData.base}"></li>
+            <li class="stats__result stats__result--${gameStatsMap.wrong}"></li>
+            <li class="stats__result stats__result--${gameStatsMap.slow}"></li>
+            <li class="stats__result stats__result--${gameStatsMap.fast}"></li>
+            <li class="stats__result stats__result--${gameStatsMap.correct}"></li>
+            <li class="stats__result stats__result--${gameStatsMap.base}"></li>
           </ul>`;
 
 export default gameStatsResult;
